2fd2b82e8c910b7620198b08af10c7ab5cb1c0be
gjpataky <gjpataky@yahoo.com>
2019-12-08 14:52:46 -0600

    added save

---
+++ Develop/server.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+const router = require('express').Router();
+const express = require("express");
+
+
+
+//const apiRoutes = require("routes");
+//const htmlRoutes = require("./routes/htmlRoutes");
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+/* get the notes */
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+router.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/notes.html'));
+    /*
+    readFileAsync('./../db/db.json', 'utf8').then(notes => {
+        console.log(notes);
+    });*/
+});
+
+router.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    console.log(req.body);
+
+    fs.readFile(__dirname + '/db/db.json', (err, data) => {
+        let notes = JSON.parse(data);
+        let newNote = req.body;
+        notes.push(newNote);
+        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            console.log('note saved');
+        });
+    });
+    res.send('post sent');
+});
+
+/*
+readFileAsync(path.join(__dirname + '/db/db.json'), 'utf8').then(notes => {
+        notes.push(newNote);
+        writeFileAsync(path.join(__dirname + '/db/db.json'), JSON.stringify(notes));
+    });
+*/
+
+/*use this instead:
+
+router.post('/notes', (req, res) => {
+  fs.readFile(__dirname + './../db/db.json', (err,data)  => {
+      convert json data to array 
+      let notes = JSON.parse(data);
+      let newNote = req.body;
+      notes.push(newNote);
+      use normal write file to save to db.json 
+      fs.writeFile(__dirname + './../db/db.json', JSON.stringify(notes), (err) => {
+        if (err) throw err;
+        console.log('note saved');
+      });
+    });
+    res.send('post sent');
+});
+module.exports = router;
+*/
+
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//console.log(path.join(__dirname, '/index.js'));
+//app.use(express.static(path.join(__dirname, '/index.js')));
+app.use("/", router);
+
+//app.use("/index.html", htmlRoutes);
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
